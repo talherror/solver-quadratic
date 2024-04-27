ca4ebb38fb62416b194719a6710e6e93eca68b46
talherror <77160880+talherror@users.noreply.github.com>
2024-04-27 20:15:50 +0300

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+import math
+
+def solve_quadratic(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root,
+    else:
+        return None
